887c601ac5e8ae8a7c1125b2f56a59024e432acd
Sergio Sanz Cebollero <scebollero@guidewire.com>
2020-12-18 22:22:10 +0100

    Stage 2/4 Words and sentences in Readability Score

--- readability/scoring/ReadabilityScorer.java
+++ readability/scoring/ReadabilityScorer.java
@@ -1,17 +1,40 @@
 package readability.scoring;
 
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class ReadabilityScorer {
+    private final static String SENTENCE_REGEX = "[.!?]";
+    private final static String WHITESPACES = "\\s+";
 
     public static Score scoreText(String text) {
-        Score score;
-        var textLength = text.length();
 
-        if (textLength > 100) {
-            score = Score.HARD;
-        } else {
-            score = Score.EASY;
-        }
+        var sentences = splitSentences(text);
+        var sentencesNumWords = countNumWordsPerSentence(sentences);
+        var wordsLengthAvg = calculateWordsCountAvg(sentencesNumWords);
 
-        return score;
+        return wordsLengthAvg > 10 ? Score.HARD : Score.EASY;
     }
+
+    private static double calculateWordsCountAvg (IntStream numWordsPerSentence) {
+        return numWordsPerSentence.average().orElse(0);
+    }
+
+    private static IntStream countNumWordsPerSentence(String[] sentences) {
+        return Stream.of(sentences)
+                .mapToInt(ReadabilityScorer::countWords);
+    }
+
+    private static int countWords (String sentence) {
+        return splitSentence(sentence).length;
+    }
+
+    private static String[] splitSentence (String sentence) {
+        return sentence.trim().split(WHITESPACES);
+    }
+
+    private static String[] splitSentences(String text) {
+        return text.split(SENTENCE_REGEX);
+    }
+
 }
